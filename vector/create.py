42291c19ffa783a9498057aa0f559fd8f1139f06
Insomnia Cafe <114473324+khalilurrrahmanridoykhan@users.noreply.github.com>
2024-09-13 22:26:36 +0600

    again

--- vector/create.py
+++ vector/create.py
@@ -14,8 +14,26 @@ matrix = np.array([
 select_al_elemtnt_vector = vector_column[1:]
 select_all_element_matrix = matrix[:2,:]
 
+# View Numner of Row & Column
+x = matrix.shape
+y = vector_row.shape
+
+# View Number of Elements ( row * column)
+size = matrix.size
+# View Numnber of Dimensions
+dimensions = matrix.ndim
+
+# Operations
+add_100 = lambda i: i + 100
+# Create Vectorized Function
+vectorized_add_100 = np.vectorize(add_100)
+# Apply function to add elements in matrix
+apply= vectorized_add_100(matrix)
+# Anonter way to add number
+anoter_add =  matrix + 100
 
 
 
 
-print(select_all_element_matrix)
+
+print(anoter_add)
