24beb38b48eb0d7752d20753fd1c0817aada9888
Insomnia Cafe <114473324+khalilurrrahmanridoykhan@users.noreply.github.com>
2024-09-14 22:52:27 +0600

    datawrangling-full

--- DataWrangling/DesribingData.py
+++ DataWrangling/DesribingData.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import numpy as np
 url = '/Users/khalilur/Documents/Python/PythonMachineLearningCookBook/DataWrangling/titanic.csv'
 dataframe = pd.read_csv(url)
 
@@ -23,5 +23,50 @@ replacefemale = dataframe['sex'].replace("female", "Woman")
 replacefemalandmaleboth = dataframe['sex'].replace(["female", "male"], ["male", "female"]).head(5)
 repaleaSingledatainwhaledataset = dataframe.replace(0, "Replaced").head(2)
 repalceregularexpressions = dataframe.replace(r"First", "1st",regex=True).head(10)
-print(repalceregularexpressions)
+
+# Rename Column 
+renamecolumn = dataframe.rename(columns={'sex': 'Gender', 'fare': 'Remane1'}).head(2)
+# max, min, sum, ave count
+max = dataframe['age'].max()
+min = dataframe['age'].min()
+sum = dataframe['age'].sum()
+count = dataframe['age'].count()
+dataframcount = dataframe.count()
+# unique Value
+uniquesex = dataframe['sex'].unique()
+valuecount = dataframe['sex'].value_counts()
+nonuniq = dataframe['age'].nunique()
+# Handling Missing Value
+missingvalue = dataframe[dataframe['age'].isnull()]
+missingvaluesex = dataframe['sex'].replace('male',np.nan)
+# Deleting a column
+deleteclumn = dataframe.drop('age', axis=1).head(2)
+deletecolumnmore = dataframe.drop(['class', 'sex'], axis=1).head(2)
+deletecolumwithoutname = dataframe.drop(dataframe.columns[1], axis=1).head(2)
+# Deleting a row
+deleterow = dataframe[dataframe['sex'] != 'male'].head(2)
+# Deleting a row with value
+deleterowwithvalue = dataframe[dataframe.index != 0].head(2)
+# Drop Duplicate rows
+dropduplicate = dataframe.drop_duplicates()
+dropsuplicatewithkeep = dataframe.drop_duplicates(subset=['sex'], keep='last')
+#  Grouping Rows by Values
+grouprow = dataframe.groupby('survived')['sex'].count()
+grouprowbyname= dataframe.groupby(['sex', 'survived'])['age'].count()
+# Looping Over THe Column
+# for sex in dataframe['sex'][0:10]:
+#     print(sex.upper())
+# Looping with list comperehensions 
+loopingwithlistcomerehensions = [who.upper() for who in dataframe['who'][0:10]]
+# Appling funcion over all elemnets in a column
+def uppercase(x):
+    return x.upper();
+finduppercase = dataframe['who'].apply(uppercase)[0:10]
+# Applying a function to groups
+fucntiontogrp = dataframe.groupby('sex').apply(lambda x:x.count())
+# concaternate two dataframe 
+# concaternate = pd.concat(dataframe, dataframe, axis=1)
+# merging datafram
+mergingdataframe = pd.merge(dataframe, dataframe, on='survived')
+print(mergingdataframe)
 
